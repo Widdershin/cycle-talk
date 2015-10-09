79103d1c13a3ccc050b57ad41ce2fdc7c42cab88
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-10 12:32:46 +1300

    Add slide navigation and test slides

---
+++ src/slide-views.js
@@ -0,0 +1,11 @@
+import {h} from '@cycle/dom';
+
+export default [
+  () => (
+    h('h1', 'Hello world!')
+  ),
+
+  () => (
+    h('h2', 'And another slide')
+  )
+];
--- src/slides.js
+++ src/slides.js
@@ -1,9 +1,42 @@
 import {Rx} from '@cycle/core';
 import {h} from '@cycle/dom';
+import _ from 'lodash';
+
+import slideViews from './slide-views';
+
+function slideNavigation (slidePosition, totalSlides) {
+  const showingFirstSlide = () => slidePosition === 0;
+  const showingLastSlide = () => slidePosition === totalSlides - 1;
+
+  return (
+    h('.slide-navigation', [
+      h('button.previous-slide', {disabled: showingFirstSlide()}, 'Previous'),
+      h('button.next-slide', {disabled: showingLastSlide()}, 'Next')
+    ])
+  );
+}
+
+function slideDeckView (slidePosition) {
+  return (
+    h('.slide-deck', [
+      h('.current-slide', `Slide #${slidePosition}`),
+      slideViews[slidePosition](),
+      slideNavigation(slidePosition, slideViews.length)
+    ])
+  );
+}
 
 export default function slides ({DOM}) {
+  const nextSlide$ = DOM.select('.next-slide').events('click');
+  const previousSlide$ = DOM.select('.previous-slide').events('click');
+
+  const slidePosition$ = Rx.Observable.merge(
+    nextSlide$.map(_ => +1),
+    previousSlide$.map(_ => -1)
+  ).scan(_.add).startWith(0);
+
   return {
-    DOM: Rx.Observable.just('Bar!')
-      .map(message => h('.message', message))
+    DOM: slidePosition$
+      .map(slidePosition => slideDeckView(slidePosition))
   };
 }
