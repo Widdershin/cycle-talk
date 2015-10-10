4689d60e17ee147b570212acb647cc92c29cca79
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-10 13:13:51 +1300

    Navigate slides with arrow keys and space

--- src/slides.js
+++ src/slides.js
@@ -2,6 +2,9 @@ import {Rx} from '@cycle/core';
 import {h} from '@cycle/dom';
 import _ from 'lodash';
 
+import {DOM} from 'rx-dom';
+const RxDOM = DOM;
+
 import slideViews from './slide-views';
 
 function slideNavigation (slidePosition, totalSlides) {
@@ -19,6 +22,22 @@ function slideNavigation (slidePosition, totalSlides) {
   );
 }
 
+function limit (operator, {min, max}) {
+  return (...args) => {
+    const result = operator(...args);
+
+    if (result < min) {
+      return min;
+    }
+
+    if (result > max) {
+      return max;
+    }
+
+    return result;
+  };
+}
+
 function slideDeckView (slidePosition) {
   const currentSlide = slideViews[slidePosition];
 
@@ -30,17 +49,29 @@ function slideDeckView (slidePosition) {
   );
 }
 
+function keyIs (...keys) {
+  return (event) => {
+    return _.include(keys, event.key) || _.include(keys, event.code);
+  };
+}
+
 export default function slides ({DOM}) {
-  const nextSlide$ = DOM.select('.next-slide').events('click');
-  const previousSlide$ = DOM.select('.previous-slide').events('click');
+  const nextSlideButton$ = DOM.select('.next-slide').events('click');
+  const previousSlideButton$ = DOM.select('.previous-slide').events('click');
+
+  const nextSlideKey$ = RxDOM.fromEvent(document.body, 'keypress')
+    .filter(keyIs('ArrowRight', 'Space'));
+
+  const previousSlideKey$ = RxDOM.fromEvent(document.body, 'keypress')
+    .filter(keyIs('ArrowLeft'));
 
   const slidePosition$ = Rx.Observable.merge(
-    nextSlide$.map(_ => +1),
-    previousSlide$.map(_ => -1)
-  ).scan(_.add).startWith(0);
+    nextSlideButton$.merge(nextSlideKey$).map(_ => +1),
+    previousSlideButton$.merge(previousSlideKey$).map(_ => -1)
+  ).scan(limit(_.add, {min: 0, max: slideViews.length - 1}))
+    .startWith(0);
 
   return {
-    DOM: slidePosition$
-      .map(slidePosition => slideDeckView(slidePosition))
+    DOM: slidePosition$.map(slideDeckView)
   };
 }
