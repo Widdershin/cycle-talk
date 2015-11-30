5f2ed649ff35563a00e09b8edbcb55c8d2e25416
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-01 12:51:22 +1300

    Fix hotloading by correctly disposing of the Cycle app

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const drivers = {
   DOM: makeDOMDriver('.app')
 };
 
-if (module.hot) module.hot.accept();
+const [sinks, sources] = Cycle.run(slides, drivers);
 
-Cycle.run(slides, drivers);
+if (module.hot) {
+  module.hot.accept();
+
+  module.hot.dispose(() => {
+    sinks.dispose();
+    sources.dispose();
+  });
+}
+
