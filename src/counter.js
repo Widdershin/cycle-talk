e12aafae26dc0f2e04fe47bd346deb0d261ec84d
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-01 18:05:06 +1300

    Rename TodoApp to CounterApp

--- src/counter.js
+++ src/counter.js
@@ -1,7 +1,7 @@
 const {Observable} = require('rx');
 const {h} = require('@cycle/dom');
 
-export default function TodoApp (DOM) {
+export default function CounterApp (DOM) {
   return {
     DOM: Observable.just(h('.hello', 'Hello world'))
   };
