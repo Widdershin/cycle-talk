124818280954b75b64bc2aaa02ddcbb02a780698
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-01 13:03:13 +1300

    Display todos

--- src/todo-app.js
+++ src/todo-app.js
@@ -1,8 +1,33 @@
 const {Observable} = require('rx');
 const {h} = require('@cycle/dom');
 
+function todoView ({todo, done}) {
+  const style = {
+    'text-decoration': done ? 'line-through' : 'normal'
+  };
+
+  return (
+    h('.todo', {key: todo, style}, todo)
+  );
+}
+
+function view ({todos}) {
+  return (
+    h('.todos', todos.map(todoView))
+  );
+}
+
 export default function TodoApp (DOM) {
+  const initialState = {
+    todos: [
+      {todo: 'display todos', done: true},
+      {todo: 'add todos', done: false}
+    ]
+  };
+
+  const state$ = Observable.just(initialState);
+
   return {
-    DOM: Observable.just(h('.hello', 'Hello world'))
+    DOM: state$.map(view)
   };
 }
